4b46129be99b54d787a4bbab7f6f8ac172801a64
Ram <ramkb466@gmail.com>
2019-08-15 16:33:36 -0400

    made some changes in css

--- js/index.js
+++ js/index.js
@@ -49,14 +49,7 @@ var data4 = [
 ];
 
 function drawPieChart(data){
-    // let per;
-    // let sum = 0;
-    // data.forEach(d => {
-    //     sum += d.percentage;
-    // })
-    //debugger
-    // per = (data.percentage / sum);
-    // console.log(per)
+    
     var svg = d3.select('.pie_chart')
         .attr("width", svgWidth)
         .attr("height", svgHeight);
@@ -67,7 +60,8 @@ function drawPieChart(data){
     
     var color = d3.scaleOrdinal(d3.schemePaired);
     
-    var pie = d3.pie().value(function (d) {
+    var pie = d3.pie()
+        .sort(null).value(function (d) {
         return d.menus_appeared;
     });
     
@@ -108,7 +102,7 @@ function drawPieChart(data){
         dish_data.slice(0, 10).forEach(d => {
             d.menus_appeared = +d.menus_appeared;
         })
-        drawPieChart(dish_data.slice(0, 6))
+        drawPieChart(dish_data.slice(0, 10))
         //console.log(dish_data.slice(0, 10))
     })
 }
@@ -169,7 +163,7 @@ function dish_testing4() {
           testing1();
           dish_testing1();
         //drawPieChart(data1)
-       
+        
         const typical = document.getElementById("typical");
         typical.onclick = () => {
             testing1();
@@ -328,6 +322,77 @@ function testing4() {
 //     console.log(dish_data.slice(0,2))
 // })
 
+// function pie_chart_testing1(data){
+//     var width = 450;
+//      var   height = 450;
+//      var margin = 40;
 
+//      var radius = Math.min(width, height)/2 - margin;
 
+//      var svg = d3.select('.pie_chart')
+//                 .append('svg')
+//                 .attr('width', width)
+//                 .attr('height', height)
+//                 .append('g')
+//                 .attr('transform', 'translate (' + width / 2 + "," + height / 2 + ")");
 
+//     var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+//     var pie = d3.pie()
+//                 .sort(null)
+//                 .value(function(d){return d.menus_appeared;})
+            
+//     var data_ready = pie(d3.entries(data));
+
+//     var arc = d3.arc()
+//     .innerRadius(radius * 0.5)
+//     .outerRadius(radius * 0.8)
+
+//     var outerArc = d3.arc()
+//         .innerRadius(radius * 0.9)
+//         .outerRadius(radius * 0.9)
+
+//     svg.selectAll("allSlices")
+//     .data(data_ready)
+//     .enter()
+//     .append('path')
+//         .attr('d', arc)
+//         .attr('fill', function (d) { return (color(d.data.key)) })
+//         .attr("stroke", "white")
+//         .style("stroke-width", "2px")
+//         .style("opacity", 0.7)
+
+//         svg
+//         .selectAll('allPolylines')
+//         .data(data_ready)
+//         .enter()
+//         .append('polyline')
+//         .attr("stroke", "black")
+//         .style("fill", "none")
+//         .attr("stroke-width", 1)
+//         .attr('points', function (d) {
+//             var posA = arc.centroid(d) 
+//             var posB = outerArc.centroid(d) 
+//             var posC = outerArc.centroid(d); 
+//             var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 
+//             posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); 
+//             return [posA, posB, posC]
+//         })
+
+//         svg
+//         .selectAll('allLabels')
+//         .data(data_ready)
+//         .enter()
+//         .append('text')
+//         .text(function (d) {return d.data.key })
+//         .attr('transform', function (d) {
+//             var pos = outerArc.centroid(d);
+//             var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+//             pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+//             return 'translate(' + pos + ')';
+//         })
+//         .style('text-anchor', function (d) {
+//             var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+//             return (midangle < Math.PI ? 'start' : 'end')
+//         })
+// }
