0a081f4afd54622b24d95729677b01a9566f9cb7
Ram <ramkb466@gmail.com>
2019-08-16 09:49:29 -0400

    changed the path

--- js/index.js
+++ js/index.js
@@ -67,7 +67,7 @@ function drawPieChart(data){
     
     var path = d3.arc()
         .outerRadius(radius)
-        .innerRadius(0);
+        .innerRadius(10);
     
     var arc = g.selectAll("arc")
         .data(pie(data))
@@ -200,8 +200,8 @@ const render = data => {
 
     const width = 300;
     const height = 500;
-    const margin = {top: 10, right: 10, bottom: 10, left: 150};
-    //const innerWidth = width - margin.left - margin.right;
+    const margin = {top: 10, right: 20, bottom: 10, left: 150};
+   // const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
     // 
     const xScale = d3.scaleLinear()
@@ -227,9 +227,18 @@ const render = data => {
     const g = svg.append("g")
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    g.append('g').call(yAxis);
+    g.append('g').call(yAxis)
+    .select('.domain','.tick line')
+    .remove();
     g.append("g").call(xAxis)
-        .attr('transform', `translate(0, ${innerHeight})`);
+        .attr('transform', `translate(0, ${innerHeight})`)
+        .append('text')
+        .attr('y', 50)
+        .attr('x', width/ 2)
+        .attr('fill', 'black')
+        .attr('font-size', 20)
+        .text('Number of Dishes');
+        
 
     chart = true;
      g.selectAll("rect").data(data)
@@ -238,7 +247,8 @@ const render = data => {
             .attr("y", d => yScale(d.location))
             .attr("width", d => xScale(d.dish_count))
             .attr("height", yScale.bandwidth());
-        
+     g.append('text')
+     .text("Number of Dishes severed in New York Hotels");   
 
 }
    function testing1(){
@@ -310,89 +320,7 @@ function testing4() {
     })
 }
 
-// let dish_data = []
-// d3.csv("../dist/dishes_data.csv").then((data) => {
-//     dish_data = data.filter(datum => {
-        
-//         return parseInt(datum.highest_price, 10) > 20
 
-//     })
 
-//     drawPieChart(dish_data.slice(0, 6))
-//     console.log(dish_data.slice(0,2))
-// })
+// testings
 
-// function pie_chart_testing1(data){
-//     var width = 450;
-//      var   height = 450;
-//      var margin = 40;
-
-//      var radius = Math.min(width, height)/2 - margin;
-
-//      var svg = d3.select('.pie_chart')
-//                 .append('svg')
-//                 .attr('width', width)
-//                 .attr('height', height)
-//                 .append('g')
-//                 .attr('transform', 'translate (' + width / 2 + "," + height / 2 + ")");
-
-//     var color = d3.scaleOrdinal(d3.schemeCategory10);
-
-//     var pie = d3.pie()
-//                 .sort(null)
-//                 .value(function(d){return d.menus_appeared;})
-            
-//     var data_ready = pie(d3.entries(data));
-
-//     var arc = d3.arc()
-//     .innerRadius(radius * 0.5)
-//     .outerRadius(radius * 0.8)
-
-//     var outerArc = d3.arc()
-//         .innerRadius(radius * 0.9)
-//         .outerRadius(radius * 0.9)
-
-//     svg.selectAll("allSlices")
-//     .data(data_ready)
-//     .enter()
-//     .append('path')
-//         .attr('d', arc)
-//         .attr('fill', function (d) { return (color(d.data.key)) })
-//         .attr("stroke", "white")
-//         .style("stroke-width", "2px")
-//         .style("opacity", 0.7)
-
-//         svg
-//         .selectAll('allPolylines')
-//         .data(data_ready)
-//         .enter()
-//         .append('polyline')
-//         .attr("stroke", "black")
-//         .style("fill", "none")
-//         .attr("stroke-width", 1)
-//         .attr('points', function (d) {
-//             var posA = arc.centroid(d) 
-//             var posB = outerArc.centroid(d) 
-//             var posC = outerArc.centroid(d); 
-//             var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 
-//             posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); 
-//             return [posA, posB, posC]
-//         })
-
-//         svg
-//         .selectAll('allLabels')
-//         .data(data_ready)
-//         .enter()
-//         .append('text')
-//         .text(function (d) {return d.data.key })
-//         .attr('transform', function (d) {
-//             var pos = outerArc.centroid(d);
-//             var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-//             pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
-//             return 'translate(' + pos + ')';
-//         })
-//         .style('text-anchor', function (d) {
-//             var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-//             return (midangle < Math.PI ? 'start' : 'end')
-//         })
-// }
