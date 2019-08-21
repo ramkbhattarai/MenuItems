f03ddf9cfc933777d5e011436da1cb7f9b617916
Ram <ramkb466@gmail.com>
2019-08-21 16:39:22 -0400

    used only data from the dish and changed the functionality and presentation model too.

--- js/index.js
+++ js/index.js
@@ -1,131 +1,15 @@
 
-let svgWidth = 500, svgHeight = 300, barPadding = 5, radius = Math.min(svgWidth, svgHeight) / 2 ;
-
-let pieChart;
-
-function drawPieChart(data){
-   
-    var svg = d3.select('.pie_chart')
-        .attr("width", svgWidth)
-        .attr("height", svgHeight);
-
-    if (pieChart) {svg.select('g').remove() }
-       
-         var g = svg.append('g')
-            .attr("transform", "translate(" + svgWidth / 2 + "," + svgHeight/2 + ")");
-
-     pieChart = true;
-        
-    var color = d3.scaleOrdinal(["#98abc5",
-        "#8a89a6",
-        "#7b6888",
-        "#6b486b",
-        "#a05d56",
-        "#d0743c",
-        "#ff8c00",
-        "#e34d01",
-        "black",
-        "#3e7eca",
-        "#aa0092",
-        "#b32e4f"]);
-
-    var pie = d3.pie()
-        .sort(null)
-        .value(function (d) { return d.menus_appeared; });
-
-  
-    var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-
-    var outerArc = d3.arc()
-        .innerRadius(radius * 0.9)
-        .outerRadius(radius * 0.9);
-
-
-    var label = d3.arc()
-        .outerRadius(radius - 40)
-        .innerRadius(radius - 40);
-
-    var arc = g.selectAll(".arc")
-        .data(pie(data))
-        .enter().append("g")
-        .attr("class", "arc");
-
-
-    
-    arc.append("path")
-        .attr("d", path)
-        .attr("fill", function (d) { return color(d.data.name); });
-
-
-    arc.append("text")
-
-        .attr("transform", function (d, i) {
-            var pos = outerArc.centroid(d);
-            pos[0] = radius * (midAngle(d) < Math.PI ? 1.1 : -1.1);
-
-
-            var percent = (d.endAngle - d.startAngle) / (2 * Math.PI) * 100
-            if (percent < 3) {
-                
-                pos[1] += i * 10
-            }
-            return "translate(" + pos + ")";
-        })
-        .text(function (d) { return d.data.name.slice(0, 30); })
-        .attr("fill", function (d, i) { return color(i); })
-        .attr("text-anchor", 'left')
-        .attr("dx", function (d) {
-            var ac = midAngle(d) < Math.PI ? 0 : -50
-            return ac
-        })
-        .attr("dy", 5)
-
-
-    function midAngle(d) {
-        return d.startAngle + (d.endAngle - d.startAngle) / 2;
-    }
-
-    var polyline = g.selectAll("polyline")
-        .data(pie(data), function (d) {
-            return d.data.name;
-        })
-        .enter()
-        .append("polyline")
-        .attr("points", function (d, i) {
-            var pos = outerArc.centroid(d);
-            pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
-            var outer = outerArc.centroid(d)
-            var percent = (d.endAngle - d.startAngle) / (2 * Math.PI) * 100
-            if (percent < 3) {
-                outer[1]
-                pos[1] += i * 10
-            }
-            
-            return [label.centroid(d), [outer[0], pos[1]], pos];
-        })
-        .style("fill", "none")
-        
-        .attr("stroke", function (d, i) { return color(i); })
-        .style("stroke-width", "1px");
-       
-
-    
-
-}
-
  function dish_testing1() {
 
     let dish_data1 = []
     
      dish_data1 = dish_data.filter(datum => {
     
-            return (parseInt(datum.first_appeared, 10) < 1900);
+            return (parseInt(datum.first_appeared, 10) < 1950);
     
         })
        
-        drawPieChart(dish_data1.slice(0, 10))
+        testing_again(dish_data1.slice(0, 10))
         //console.log(dish_data.slice(0, 10))
     
 }
@@ -140,7 +24,7 @@ function dish_testing2() {
 
         })
        
-        drawPieChart(dish_data2.slice(0, 6))
+    testing_again(dish_data2.slice(0, 6))
         //console.log(dish_data.slice(0, 10))
    
 }
@@ -155,7 +39,7 @@ function dish_testing3() {
 
         })
         
-        drawPieChart(dish_data3.slice(0, 6))
+    testing_again(dish_data3.slice(0, 6))
         //console.log(dish_data.slice(0, 10))
     
 }
@@ -170,166 +54,304 @@ function dish_testing4() {
 
         })
         
-        drawPieChart(dish_data4.slice(0, 6))
+    testing_again(dish_data4.slice(0, 6))
         //console.log(dish_data.slice(0, 10))
    
 }
 
 
     document.addEventListener("DOMContentLoaded", () => {
-          testing1();
+         // testing1();
           dish_testing1();
         //drawPieChart(data1)
         
         const typical = document.getElementById("typical");
         typical.onclick = () => {
-            testing1();
+           // testing1();
             dish_testing1();
            // drawPieChart(data1)
         }
-        const regular = document.getElementById("regular");
-        regular.onclick = () => {
+        // const regular = document.getElementById("regular");
+        // regular.onclick = () => {
            
-            testing2();
-            dish_testing2();
-           // drawPieChart(data2);
-        }
+        //    // testing2();
+        //     dish_testing2();
+        //    // drawPieChart(data2);
+        // }
         const special = document.getElementById("special");
         special.onclick = () => {
-            testing3();
+            //testing3();
             dish_testing3();
            // drawPieChart(data3)
         }
         const newari = document.getElementById("newari");
         newari.onclick = () => {
-            testing4();
+           // testing4();
             dish_testing4();
            // drawPieChart(data4)
         }
     })
-
-    
-let chart;
-
-const render = data => {
-    
-    // d3.select(".bar-chart").remove();
-
-    const width = 300;
-    const height = 500;
-    const margin = {top: 20, right: 20, bottom: 10, left: 150};
-   // const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-    // 
-    const xScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.dish_count)])
-        .range([0, width]);
-
-        const xAxis = d3.axisBottom(xScale);
-    const yScale = d3.scaleBand()
-        .domain(data.map(d => d.location))
-        .range([0, innerHeight])
-        .padding(0.1);
-        //console.log(yScale.domain())
-     const yAxis = d3.axisLeft(yScale);
-    
-    let svg = d3.select(".bar-chart");
-    
-    
-
-    if (chart){
-        svg.select('g').remove();
-    }
-
-    const g = svg.append("g")
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-    g.append('g').call(yAxis)
-    .select('.domain','.tick line')
-    .remove();
-    g.append("g").call(xAxis)
-        .attr('transform', `translate(0, ${innerHeight})`)
-        .append('text')
-        .attr('y', 50)
-        .attr('x', width/ 2)
-        .attr('fill', 'black')
-        .attr('font-size', 20)
-        .text('Number of Dishes');
-        
-
-    chart = true;
-     g.selectAll("rect").data(data)
-        .enter()
-        .append("rect")
-            .attr("y", d => yScale(d.location))
-            .attr("width", d => xScale(d.dish_count))
-            .attr("height", yScale.bandwidth());
-     g.append('text')
-     .text("Number of Dishes severed in New York Hotels");   
-
-}
-   function testing1(){
-
-       let menu_data1 = []
-         
-       menu_data1 = menu_data.filter(datum => {
-                  
-                return (parseInt(datum.date, 10) < 1900 && datum.place === "NEW YORK");
-       
-            })
-           
-
-             render(menu_data1.slice(0, 10))
-            // console.log(menu_data.slice(0,10)[0].dish_count)
-      
-   }
-
-function testing2() {
-
-    let menu_data2 = []
-   
-    menu_data2 = menu_data.filter(datum => {
-
-            return (parseInt(datum.date, 10) > 1900 && parseInt(datum.date, 10) < 1950  && datum.place === "NEW YORK");
-
-        })
-       
-        render(menu_data2.slice(0, 10))
-        //console.log(menu_data.slice(0,10))
-   
-}
-
-function testing3() {
-
-    let menu_data3 = []
-    
-        menu_data3 = menu_data.filter(datum => {
-
-            return (parseInt(datum.date, 10) > 1950 && parseInt(datum.date, 10) < 1980 ); // not in new york
-
-        })
-       
-        render(menu_data3.slice(0, 10))
-        // console.log(menu_data.slice(0,10))
-   
-}
-
-function testing4() {
-
-    let menu_data4 = []
-    
-    menu_data4 = menu_data.filter(datum => {
-
-            return (parseInt(datum.date, 10) > 2000 );// not in new york
-
-        })
-        
-        render(menu_data4.slice(0, 10))
-        // console.log(menu_data.slice(0,10))
-   
-}
-
-
-
 // testings
 
+let histogramdata;
+let piechartdata;
+let legenddata;
+
+function dashboard(id, fData) {
+    var barColor = 'steelblue';
+    function segColor(c) { return { low: "#807dba", mid: "#e08214", high: "#41ab5d" }[c]; }
+
+    // compute total for each Name.
+    fData.forEach(function (d) { d.total = d.freq.low + d.freq.mid + d.freq.high; });
+
+    // function to handle histogram.
+    function histoGram(fD) {
+        var hG = {}, hGDim = { t: 60, r: 0, b: 30, l: 0 };
+        hGDim.w = 500 - hGDim.l - hGDim.r,
+            hGDim.h = 300 - hGDim.t - hGDim.b;
+
+            if(histogramdata){
+                d3.select(id).selectAll("svg").remove();
+            }
+        //create svg for histogram.
+        var hGsvg = d3.select(id).append("svg")
+            .attr("width", hGDim.w + hGDim.l + hGDim.r)
+            .attr("height", hGDim.h + hGDim.t + hGDim.b).append("g")
+            .attr("transform", "translate(" + hGDim.l + "," + hGDim.t + ")");
+            histogramdata = true;
+        // create function for x-axis mapping.
+        var x = d3.scale.ordinal().rangeRoundBands([0, hGDim.w], 0.1)
+            .domain(fD.map(function (d) { return d[0]; }));
+
+        // Add x-axis to the histogram svg.
+        hGsvg.append("g").attr("class", "x axis")
+            .attr("transform", "translate(0," + hGDim.h + ")")
+            .call(d3.svg.axis().scale(x).orient("bottom"))
+            .selectAll(".tick text")
+            .call(wrap, x.rangeBand());
+
+            function wrap(text, width) {
+            text.each(function() {
+                var text = d3.select(this),
+                    words = text.text().split(/\s+/).reverse(),
+                    word,
+                    line = [],
+                    lineNumber = 0,
+                    lineHeight = 1.1, // ems
+                    y = text.attr("y"),
+                    dy = parseFloat(text.attr("dy")),
+                    tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+                while (word = words.pop()) {
+                line.push(word);
+                tspan.text(line.join(" "));
+                if (tspan.node().getComputedTextLength() > width) {
+                    line.pop();
+                    tspan.text(line.join(" "));
+                    line = [word];
+                    tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                }
+                }
+            });
+            }
+
+        // Create function for y-axis map.
+        var y = d3.scale.linear().range([hGDim.h, 0])
+            .domain([0, d3.max(fD, function (d) { return d[1]; })]);
+
+        // Create bars for histogram to contain rectangles and freq labels.
+        var bars = hGsvg.selectAll(".bar").data(fD).enter()
+            .append("g").attr("class", "bar");
+
+        //create the rectangles.
+        bars.append("rect")
+            .attr("x", function (d) { return x(d[0]); })
+            .attr("y", function (d) { return y(d[1]); })
+            .attr("width", x.rangeBand())
+            .attr("height", function (d) { return hGDim.h - y(d[1]); })
+            .attr('fill', barColor)
+            .on("mouseover", mouseover)// mouseover is defined below.
+            .on("mouseout", mouseout);// mouseout is defined below.
+
+        
+        //Create the frequency labels above the rectangles.
+        bars.append("text").text(function (d) { 
+            var datum = (d3.format(",")(d[1])).slice(0, 4);
+           
+            return datum })
+            .attr("x", function (d) { return x(d[0]) + x.rangeBand() / 2; })
+            .attr("y", function (d) { return y(d[1]) - 5; })
+            .attr("text-anchor", "middle");
+
+        function mouseover(d) {  // utility function to be called on mouseover.
+            // filter for selected Name.
+            var st = fData.filter(function (s) { return s.Name == d[0]; })[0],
+                nD = d3.keys(st.freq).map(function (s) { return { type: s, freq: st.freq[s] }; });
+
+            // call update functions of pie-chart and legend.    
+            pC.update(nD);
+            leg.update(nD);
+        }
+
+        function mouseout(d) {    // utility function to be called on mouseout.
+            // reset the pie-chart and legend.    
+            pC.update(tF);
+            leg.update(tF);
+        }
+
+        // create function to update the bars. This will be used by pie-chart.
+        hG.update = function (nD, color) {
+            // update the domain of the y-axis map to reflect change in frequencies.
+            y.domain([0, d3.max(nD, function (d) { return d[1]; })]);
+
+            // Attach the new data to the bars.
+            var bars = hGsvg.selectAll(".bar").data(nD);
+
+            // transition the height and color of rectangles.
+            bars.select("rect").transition().duration(500)
+                .attr("y", function (d) { return y(d[1]); })
+                .attr("height", function (d) { return hGDim.h - y(d[1]); })
+                .attr("fill", color);
+
+            // transition the frequency labels location and change value.
+            bars.select("text").transition().duration(500)
+                .text(function (d) { return (d3.format(",")(d[1])).slice(0,4) })
+                .attr("y", function (d) { return y(d[1]) - 5; });
+        }
+        return hG;
+    }
+
+    // function to handle pieChart.
+    function pieChart(pD) {
+        var pC = {}, pieDim = { w: 250, h: 250 };
+        pieDim.r = Math.min(pieDim.w, pieDim.h) / 2;
+
+        // if (piechartdata) {
+        //     d3.select(id).selectAll("svg").remove();
+        // }
+        // create svg for pie chart.
+        var piesvg = d3.select(id).append("svg")
+            .attr("width", pieDim.w).attr("height", pieDim.h).append("g")
+            .attr("transform", "translate(" + pieDim.w / 2 + "," + pieDim.h / 2 + ")");
+
+            // piechartdata = true;
+        // create function to draw the arcs of the pie slices.
+        var arc = d3.svg.arc().outerRadius(pieDim.r - 10).innerRadius(0);
+
+        // create a function to compute the pie slice angles.
+        var pie = d3.layout.pie().sort(null).value(function (d) { return d.freq; });
+
+        // Draw the pie slices.
+        piesvg.selectAll("path").data(pie(pD)).enter().append("path").attr("d", arc)
+            .each(function (d) { this._current = d; })
+            .style("fill", function (d) { return segColor(d.data.type); })
+            .on("mouseover", mouseover).on("mouseout", mouseout);
+
+        // create function to update pie-chart. This will be used by histogram.
+        pC.update = function (nD) {
+            piesvg.selectAll("path").data(pie(nD)).transition().duration(500)
+                .attrTween("d", arcTween);
+        }
+        // Utility function to be called on mouseover a pie slice.
+        function mouseover(d) {
+            // call the update function of histogram with new data.
+            hG.update(fData.map(function (v) {
+                return [v.Name, v.freq[d.data.type]];
+            }), segColor(d.data.type));
+        }
+        //Utility function to be called on mouseout a pie slice.
+        function mouseout(d) {
+            // call the update function of histogram with all data.
+            hG.update(fData.map(function (v) {
+                return [v.Name, v.total];
+            }), barColor);
+        }
+        // Animating the pie-slice requiring a custom function which specifies
+        // how the intermediate paths should be drawn.
+        function arcTween(a) {
+            var i = d3.interpolate(this._current, a);
+            this._current = i(0);
+            return function (t) { return arc(i(t)); };
+        }
+        return pC;
+    }
+
+    // function to handle legend.
+    function legend(lD) {
+        var leg = {};
+
+        if (legenddata) {
+            d3.select(id).selectAll("table").remove();
+        }
+        // create table for legend.
+        var legend = d3.select(id).append("table");
+        legend.attr('class', 'legend');
+
+        // create one row per segment.
+        var tr = legend.append("tbody").selectAll("tr").data(lD).enter().append("tr");
+
+        // create the first column for each segment.
+        tr.append("td").append("svg").attr("width", '16').attr("height", '16').append("rect")
+            .attr("width", '16').attr("height", '16')
+            .attr("fill", function (d) { return segColor(d.type); });
+
+            legenddata = true;
+        // create the second column for each segment.
+        tr.append("td").text(function (d) { return d.type; });
+
+        // create the third column for each segment.
+        tr.append("td").attr("class", 'legendFreq')
+            .text(function (d) { return d3.format(",")(d.freq); });
+
+        // create the fourth column for each segment.
+        tr.append("td").attr("class", 'legendPerc')
+            .text(function (d) { return getLegend(d, lD); });
+
+        // Utility function to be used to update the legend.
+        leg.update = function (nD) {
+            // update the data attached to the row elements.
+            var l = legend.select("tbody").selectAll("tr").data(nD);
+
+            // update the frequencies.
+            l.select(".legendFreq").text(function (d) { return d3.format(",")(d.freq); });
+
+            // update the percentage column.
+            l.select(".legendPerc").text(function (d) { return getLegend(d, nD); });
+        }
+
+        function getLegend(d, aD) { // Utility function to compute percentage.
+            return d3.format("%")(d.freq / d3.sum(aD.map(function (v) { return v.freq; })));
+        }
+
+        return leg;
+    }
+
+    // calculate total frequency by segment for all Name.
+    var tF = ['low', 'mid', 'high'].map(function (d) {
+        return { type: d, freq: d3.sum(fData.map(function (t) { return t.freq[d]; })) };
+    });
+
+    // calculate total frequency by Name for all segment.
+    var sF = fData.map(function (d) { return [d.Name, d.total]; });
+
+    var hG = histoGram(sF), // create the histogram.
+        pC = pieChart(tF), // create the pie-chart.
+        leg = legend(tF);  // create the legend.
+
+     
+}
+
+        var freqData;
+  function testing_again (data) {
+            freqData = data.map(function (d) {
+                return {
+                    Name: d.name,
+                    freq: {
+                        low: d.lowest_price,
+                        mid: (d.lowest_price + d.highest_price)/2,
+                        high: d.highest_price
+                    }
+                }
+            });
+  dashboard('#dashboard',freqData);
+      };
+      
